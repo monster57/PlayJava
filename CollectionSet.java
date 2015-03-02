43423d8786eb9e04eabc8909b81d9bab08bb7e0e
monster57 <surajitb@thoughtworks.com>
2015-03-02 10:54:27 +0530

    set is created

---
+++ CollectionSet.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+class CollectionSet{
+	public static void main(String[] args) {
+		int count[] = {34,33,22,14,23,12};
+		Set<Integer> newSet = new HashSet<Integer>();
+		try{
+			for(int i = 0;i<5;i++){
+				newSet.add(count[i]);
+			}
+			System.out.println("the set elements are \t" +newSet);
+		}
+		catch(Exception e){
+
+		}
+	}
+}
