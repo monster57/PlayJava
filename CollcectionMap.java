299b8fdb9eb3ea738712e590e126d3f69f394e63
monster57 <surajitb@thoughtworks.com>
2015-03-02 10:42:01 +0530

    tried with map

---
+++ CollcectionMap.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+class Collection{
+	public static void main(String[] args) {
+		Map one = new HashMap();
+		one.put("jp" , "21");
+		one.put("Nandu" , "22");
+		one.put("supu" , "21");
+		System.out.println();
+		System.out.println(" Map Elements are");
+		System.out.println(" \t"+one);
+
+	}
+}
